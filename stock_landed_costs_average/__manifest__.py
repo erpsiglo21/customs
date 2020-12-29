ee33bdff954cda10868fc93ae9f485d2cb9fc6de
rfloresl <r.flores.l@gmail.com>
2020-12-29 17:37:05 -0300

    Commit inicial

---
+++ stock_landed_costs_average/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Landed Costs Average',
+    'summary': 'Use Landed Costs on Average Cost inventory.',
+    'version': '1.0.14',
+    'author': 'ERP Siglo 21',
+    'category': 'Warehouse',
+    'license': 'AGPL-3',
+    'depends': [
+        'stock_landed_costs',
+    ],
+    'demo': [],
+    'data': [
+    ],
+    'auto_install': False,
+    'installable': True,
+}
---
+++ stock_landed_costs_tags/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Landed costs tags',
+    'summary': 'Retrieves the bills on landed costs depending on analityc tags.',
+    'version': '1.0.14',
+    'author': 'ERP Siglo 21',
+    'category': 'Warehouse',
+    'license': 'AGPL-3',
+    'data': [
+        'data/res_config.xml',
+        'data/res_users_data.xml',
+        'views/stock_landed_cost_views.xml',
+        'views/account_move_views.xml',
+    ],
+    'demo': [
+        'demo/product_demo.xml',
+        'demo/account_move_demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
